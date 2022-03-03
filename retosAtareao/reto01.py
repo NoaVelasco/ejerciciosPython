6030eea08e3f26c6d7c5bfc7ae2dc4da700b32bd
NoaVelasco <noavelasco@gmail.com>
2022-03-03 03:31:30 +0100

    220302 Reto 02 conseguido

--- retosAtareao/reto01.py
+++ retosAtareao/reto01.py
@@ -6,7 +6,7 @@ def get_download_folder():
     
 print(get_download_folder()) 
 
-ejemplo_dir = '/Users/noave/Downloads'
+ejemplo_dir = get_download_folder()
 with os.scandir(ejemplo_dir) as ficheros:
     ficheros = [fichero.name for fichero in ficheros if fichero.is_file()]
 for i in ficheros:

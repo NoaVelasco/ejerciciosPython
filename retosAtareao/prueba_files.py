3c94e2e0580aeb695a50d9e79a7174aef095e61c
NoaVelasco <noavelasco@gmail.com>
2022-03-20 19:51:14 +0100

    220320 reto3 mejorado

--- retosAtareao/prueba_files.py
+++ retosAtareao/prueba_files.py
@@ -1,5 +1,6 @@
-import mimetypes
-from os import *
+#import mimetypes
+#from os import *
+import re
 
 
 """ruta2 = "/TRABAJO/_Programación/"
@@ -12,13 +13,17 @@ with open("/TRABAJO/_Programación/prueba2.txt", "w") as f:
     f.close()
 """
 
-
-directorio = "/Users/noave/Downloads"
+"""directorio = "/Users/noave/Downloads"
 listArchivos = listdir(directorio)
 for fichero in listArchivos:
     if mimetypes.guess_type(fichero)[0] == "image/jpeg":
-        print(fichero)
+        print(fichero)"""
 
 """for fichero in listArchivos:
     if path.isfile(path.join(directorio, fichero)):
         print(fichero)"""
+
+leido='directorio = "/Users/noave/Downloads"'
+patron=re.compile(r'"(\S+)"')
+
+print(re.findall(patron, leido))
--- retosAtareao/reto03.py
+++ retosAtareao/reto03.py
@@ -1,42 +1,47 @@
 # Python 3.10.2 UTF-8
 # Copyright (c) 2022, Noa Velasco
 
-# Para el proyecto "reto-python" 
-# de Lorenzo Carbonell <a.k.a. atareao>
+# Para el proyecto "reto-python" de Lorenzo Carbonell <a.k.a. atareao>
 
-import mimetypes
-from os import listdir, path
-import re
+import mimetypes  # Para los JPG
+from os import listdir, path  # Para manejar rutas
+import re  # Para buscar o reemplazar
+
+# No tengo la menor idea de qué es lo de TOML y no sé
+# si hay que implementar algo o cómo hacerlo.
+
 
 def main():
-    
+
     rutaDiogenes = path.join(path.expanduser(
         "~"), '.config\diogenes\diogenes.conf')
 
-    if path.isfile(rutaDiogenes):
-        with open(rutaDiogenes, "r") as f:
-            print(f.read())
+    # ¿Existe el archivo config? Si no, se crea:
 
-    else:
+    if path.isfile(rutaDiogenes) == False:
         with open(rutaDiogenes, "w") as f:
             f.write('directorio = "/Users/noave/Downloads"')
-            
+
+    # Se lee el archivo:
 
     with open(rutaDiogenes, "r") as f:
         dwnldDir = f.readline()
 
-    reemplazo1 = re.sub(r"""[ ="]""", "", dwnldDir)
-    reemplazo2 = re.sub(r"""directorio""", "", reemplazo1) 
+    # Aisla el directorio con expresiones regulares:
+    
+    patron = re.compile(r'"(\S+)"')
+    reemplazo3=re.findall(patron, dwnldDir)
 
-    listArchivos = listdir(reemplazo2)
+    print(
+        f"Imágenes JPG en el directorio {reemplazo3[0]}: \n-----------------------------")
+
+    # Si los archivos del directorio son JPG, mostrarlos uno a uno:
+
+    listArchivos = listdir(reemplazo3[0])
     for fichero in listArchivos:
         if mimetypes.guess_type(fichero)[0] == "image/jpeg":
             print(fichero)
-        
- 
+
 
 if __name__ == "__main__":
     main()
-
-
-
